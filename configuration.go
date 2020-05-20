0558ea61bc734fe85776cb5d2208c50775ce6675
Ken Lam <Ken.Lam@moodys.com>
2020-05-20 11:08:15 -0700

    add ingore feature and fallBackDirectory if required

--- configuration.go
+++ configuration.go
@@ -2,7 +2,11 @@ package configuration
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/go-akka/configuration/hocon"
 )
@@ -18,7 +22,59 @@ func ParseString(text string, includeCallback ...hocon.IncludeCallback) *Config 
 	return NewConfigFromRoot(root)
 }
 
-func LoadConfig(filename string) *Config {
+func checkFileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// func directoryFallBack(filename string) string {
+// 	splitDirAndFile := strings.Split(filename, "../")
+// 	dirLevel := len(splitDirAndFile)
+// 	newfile := func() string {
+// 		switch {
+// 			case dirLevel > 2:
+// 				return strings.Join(splitDirAndFile[1:], "../")
+// 			case dirLevel == 2:
+// 				return strings.Join(splitDirAndFile, "./")
+// 			default:
+// 				return filename
+// 			}
+// 	}
+// 	fileExists := checkFileExists(newfile())
+// 	if (fileExists(newfile())) == true {
+// 		return newfile()
+// 	} else {
+// 		return filename
+// 	}
+
+// }
+
+func directoryFallBack(filename string) string {
+	splitDirAndFile := strings.Split(filename, "../")
+	dirLevel := len(splitDirAndFile)
+	switch {
+	case dirLevel > 2:
+		return strings.Join(splitDirAndFile[1:], "../")
+	case dirLevel == 2:
+		return strings.Join(splitDirAndFile, "./")
+	default:
+		return filename
+	}
+}
+
+func LoadConfig(filename string, upperDirCheck bool) *Config {
+	if upperDirCheck == true {
+		newFile := directoryFallBack(filename)
+		if checkFileExists(newFile) == false {
+			log.Println("Cannot Found the Configuration file - %s", filename)
+			log.Println("Fall back to orginal file directory")
+		} else {
+			filename = newFile
+		}
+	}
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
@@ -39,7 +95,7 @@ func FromObject(obj interface{}) *Config {
 func defaultIncludeCallback(filename string) *hocon.HoconRoot {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		fmt.Println("ignore the include file: ", filename)
 	}
 
 	return hocon.Parse(string(data), defaultIncludeCallback)
